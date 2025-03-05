2f371f9914f376a8f8994efbde188a9b514f81b3
Rica Ferrancullo <ricamari0724@gmail.com>
2025-03-05 23:02:43 +0800

    Updated main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,13 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  skip_credentials_validation = true
+  skip_metadata_api_check = true
+  skip_requesting_account_id = true
   
+  endpoints {
+    ec2 = "http://localhost:4566"
+  }
 }
 
 resource "aws_instance" "web-server" {
